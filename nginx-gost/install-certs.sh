9ed9ece9b791ead497e947f19744faa17090999e
fullincome <efremofstepan@yandex.ru>
2017-12-02 16:36:33 +0300

    choice name cert

--- nginx-gost/install-certs.sh
+++ nginx-gost/install-certs.sh
@@ -1,23 +1,39 @@
 #!/bin/bash -x
 
+certname='srvtest'
+
+if [ -n "$1" ]
+then
+	certname=$1
+fi
+
+/opt/cprocsp/bin/amd64/certmgr -list | grep 'HDIMAGE\\\\ngxtest'
+
+if [ $? -eq 0 ]
+then
+	/opt/cprocsp/bin/amd64/certmgr -delete -container '\\.\HDIMAGE\ngxtest'
+fi
+
 # Генерация тестового сертефиката:
-/opt/cprocsp/bin/amd64/cryptcp -creatcert -provtype 81 -provname 'Crypto-Pro GOST R 34.10-2012 KC1 Strong CSP' -rdn 'CN=srvtest' -cont '\\.\HDIMAGE\srvtest' -certusage 1.3.6.1.5.5.7.3.1 -ku -du -ex -ca http://cryptopro.ru/certsrv
+/opt/cprocsp/bin/amd64/cryptcp -creatcert -provtype 81 -provname 'Crypto-Pro GOST R 34.10-2012 KC1 Strong CSP' -rdn "CN=${certname}" -cont "\\\\.\\HDIMAGE\\ngxtest" -certusage 1.3.6.1.5.5.7.3.1 -ku -du -ex -ca http://cryptopro.ru/certsrv
 
 # Смена KC1 на KC2 в имени провайдера, так как nginx работает с провайдером KC2:
-/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -cont '\\.\HDIMAGE\srvtest' -provtype 75 -provname "Crypto-Pro GOST R 34.10-2001 KC2 CSP" || exit 1
+/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -cont '\\.\HDIMAGE\ngxtest' -provtype 75 -provname "Crypto-Pro GOST R 34.10-2001 KC2 CSP" || exit 1
 
 # Экспорт сертификата:
-/opt/cprocsp/bin/amd64/certmgr -export -cert -dn "CN=srvtest" -dest '/etc/nginx/srvtest.cer' || exit 1
+/opt/cprocsp/bin/amd64/certmgr -export -cert -dn "CN=${certname}" -dest "/etc/nginx/${certname}.cer" || exit 1
 
 # Смена кодировкии сертификата DER на PEM:
-openssl x509 -inform DER -in "/etc/nginx/srvtest.cer" -out "/etc/nginx/srvtest.pem" || exit 1
+openssl x509 -inform DER -in "/etc/nginx/${certname}.cer" -out "/etc/nginx/${certname}.pem" || exit 1
 
 # Генерация сертификатов RSA:
-openssl req -x509 -newkey rsa:2048 -keyout /etc/nginx/srvtestRSA.key -nodes -out /etc/nginx/srvtestRSA.pem -subj '/CN=srvtestRSA/C=RU' || exit 1
-openssl rsa -in /etc/nginx/srvtestRSA.key -out /etc/nginx/srvtestRSA.key
+openssl req -x509 -newkey rsa:2048 -keyout /etc/nginx/${certname}RSA.key -nodes -out /etc/nginx/srvtestRSA.pem -subj '/CN=${certname}RSA/C=RU' || exit 1
+openssl rsa -in /etc/nginx/srvtestRSA.key -out /etc/nginx/${certname}RSA.key
 
 # Загрузка файла конфигурации:
-wget "https://raw.githubusercontent.com/fullincome/scripts/master/nginx-gost/nginx.conf" || exit 1
+wget --no-check-certificate "https://raw.githubusercontent.com/fullincome/scripts/master/nginx-gost/nginx.conf" || exit 1
 
 # Установка конфигурации nginx:
-mv ./nginx.conf /etc/nginx/nginx.conf || exit 1
+sed -r "s/srvtest/${certname}/g" nginx.conf > nginx_tmp.conf
+rm nginx.conf
+mv ./nginx_tmp.conf /etc/nginx/nginx.conf || exit 1
