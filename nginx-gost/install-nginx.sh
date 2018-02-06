54f3b3bd8382fadd8f67899a441466faeca856b5
fullincome <efremofstepan@yandex.ru>
2018-02-06 19:52:58 +0300

    feature: add some flags

--- nginx-gost/install-nginx.sh
+++ nginx-gost/install-nginx.sh
@@ -1,66 +1,64 @@
-#!/bin/bash -x
+#!/bin/bash +x
 
-# Настройка необходимых пакетов
-# ----------------------------------
+ARGV=$@
+WORK_PATH=$(pwd)
+command_list=false
+csp_need=false
+openssl_need=false
+git_need=false
+gcc_need=false
+zlib_need=false
+pcre_need=false
+nginx_need=false
 
 # Пакеты будут скачены с "$url"
 url="https://update.cryptopro.ru/support/nginx-gost"
 
-revision_openssl="164682"
+revision_openssl="168667"
 pcre_ver="pcre-8.41"
 zlib_ver="zlib-1.2.11"
 
 # Версия nginx для загрузки с github
 nginx_branch="stable-1.12"
-no_exec=false
 
-if [ -n "$1" ] 
-then
-    if [ "$1" == "command_list" ]
-    then
-        no_exec=true
-    fi
-    csp=$1
-else
-    printf "No argument (CSP)"
-    exit 0
-fi
-
-cat /etc/*release* | grep -Ei "(centos|red hat)"
+# Определение команд под систему
+cat /etc/*release* | grep -Ei "(centos|red hat)" > /dev/null
 if [ "$?" -eq 0 ] 
 then
     apt="yum -y"
     pkgmsys="rpm"
     pkglist="rpm -qa"
     install="rpm -i"
-    openssl_packages=(cprocsp-cpopenssl-110-base-4.0.0-5.noarch.rpm \
-    cprocsp-cpopenssl-110-64-4.0.0-5.x86_64.rpm \
-    cprocsp-cpopenssl-110-devel-4.0.0-5.noarch.rpm \
-    cprocsp-cpopenssl-110-gost-64-4.0.0-5.x86_64.rpm)
+    openssl_packages=(cprocsp-cpopenssl-110-base-5.0.10976-5.noarch.rpm \
+    cprocsp-cpopenssl-110-64-5.0.10976-5.x86_64.rpm \
+    cprocsp-cpopenssl-110-devel-5.0.10976-5.noarch.rpm \
+    cprocsp-cpopenssl-110-gost-64-5.0.10976-5.x86_64.rpm)
 
     modules_path=/usr/lib64/nginx/modules
     cc_ld_opt=" --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic -fPIC' --with-ld-opt='-Wl,-z,relro -Wl,-z,now -pie'" 
 
 else
-    cat /etc/*release* | grep -Ei "(ubuntu|debian)"
+    cat /etc/*release* | grep -Ei "(ubuntu|debian)" > /dev/null
     if [ "$?" -eq 0 ] 
     then
         apt="apt-get"
         pkgmsys="deb"
         pkglist="dpkg-query --list"
         install="dpkg -i"
-        openssl_packages=(cprocsp-cpopenssl-110-base_4.0.0-5_all.deb \
-        cprocsp-cpopenssl-110-64_4.0.0-5_amd64.deb \
-        cprocsp-cpopenssl-110-devel_4.0.0-5_all.deb \
-        cprocsp-cpopenssl-110-gost-64_4.0.0-5_amd64.deb)
+        openssl_packages=(cprocsp-cpopenssl-110-base_5.0.10976-5_all.deb \
+        cprocsp-cpopenssl-110-64_5.0.10976-5_amd64.deb \
+        cprocsp-cpopenssl-110-devel_5.0.10976-5_all.deb \
+        cprocsp-cpopenssl-110-gost-64_5.0.10976-5_amd64.deb)
 
         modules_path=/usr/lib/nginx/modules
         cc_ld_opt=" --with-cc-opt='-g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fPIC' --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,-z,now -Wl,--as-needed -pie'"
     else
-        printf "Not supported system (supported: Ubuntu, CentOS, Red Hat)."
+        printf "Not supported system (supported: Ubuntu, Debian, CentOS, Red Hat).\n"
         exit 0
     fi
 fi
+# ----------------------------------------------
+
 
 prefix=/etc/nginx
 sbin_path=/usr/sbin/nginx
@@ -78,133 +76,352 @@ user=root
 group=nginx
 
 
-# ----------------------------------
-
 # Настройка установочной конфигурации nginx
-# ----------------------------------
-
-
 nginx_paths=" --prefix=${prefix} --sbin-path=${sbin_path} --modules-path=${modules_path} --conf-path=${conf_path} --error-log-path=${err_log_path} --http-log-path=${http_log_path} --http-client-body-temp-path=${http_client_body_temp_path} --http-proxy-temp-path=${http_proxy_temp_path} --http-fastcgi-temp-path=${http_fastcgi_temp_path} --http-uwsgi-temp-path=${http_uwsgi_temp_path} --http-scgi-temp-path=${http_scgi_temp_path} --pid-path=${pid_path} --lock-path=${lock_path}"
-
 nginx_parametrs=" --user=${user} --group=${group} --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module"
-
-
 # Возможны и другие модули для которых требуется самостоятельная установка пакетов, например:
 # --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_geoip_module=dynamic
 # --with-http_perl_module=dynamic
+# ----------------------------------------------
 
-# ----------------------------------
 
 
-# Загрузка, распаковка и установка пакетов
-# ----------------------------------
-if [ $no_exec == true ]
+
+
+
+
+
+
+
+
+# ----------------------------------------------
+# -----------Проверка аргументов,---------------
+# ------определение необходимых пакетов---------
+# ----------------------------------------------
+
+# Проверка аргументов и CSP
+for arg_cur in ${ARGV};
+do
+    term="`echo ${arg_cur}|awk -F= '/^\-\-.+=.+/{print $1}'`"
+    define="`echo ${arg_cur}|awk -F= '/^\-\-.+=.+/{print $2}'`"
+    case ${term} in
+        # Проверка CSP
+        "--csp")
+            csp_need=true
+            csp=${define}
+            ;;
+    esac
+
+    # Проверка command list
+    if echo ${arg_cur}|grep "\-\-command_list" > /dev/null
+    then
+        command_list=true
+    fi
+    # Проверка force install
+    if echo ${arg_cur}|grep "\-\-force\-install" > /dev/null
+    then
+        openssl_need=true
+        nginx_need=true
+    fi
+done
+
+if [ ${csp_need} == false ] 
 then
-# Вывод комманд
-# ----------------------------------
-    echo "This commands will be carry out:" > command_list
-
-    eval "$pkglist | grep -qw gcc"
-    if ! [ "$?" -eq 0 ]
+    if (eval "${pkglist} | grep cprocsp-base > /dev/null" && \
+        eval "${pkglist} | grep cprocsp-capilite > /dev/null" && \
+        eval "${pkglist} | grep cprocsp-kc1 > /dev/null" &&
+        eval "${pkglist} | grep cprocsp-kc1 > /dev/null")
     then
-        echo "$apt install gcc" >> command_list
-    fi
-    eval "$pkglist | grep \" git \""
-    if ! [ "$?" -eq 0 ]
-    then
-        echo "$apt install git" >> command_list
-    fi
-
-    echo "wget --no-check-certificate -O nginx_conf.patch https://raw.githubusercontent.com/fullincome/scripts/master/nginx-gost/nginx_conf.patch" >> command_list
-    echo "wget --no-check-certificate -O ${pcre_ver}.tar.gz ${url}/src/${pcre_ver}.tar.gz && wget --no-check-certificate -O ${zlib_ver}.tar.gz ${url}/src/${zlib_ver}.tar.gz" >> command_list
-    for i in ${openssl_packages[@]}; do echo "wget --no-check-certificate -O $i ${url}/bin/${revision_openssl}/$i" >> command_list; done
-
-    echo "tar -xzvf ${pcre_ver}.tar.gz && tar -xzvf ${zlib_ver}.tar.gz" >> command_list
-    cmd=$install" lsb-cprocsp-kc2*"${pkgmsys}
-    if ! [ -d "$csp" ]
-    then
-        if ! [ -d csp ]
-        then
-            echo "mkdir csp" >> command_list
-        fi
-        echo "tar -xzvf $csp -C csp --strip-components 1" >> command_list
-        csp="csp"
-    fi
-
-    echo "cd ${csp} && ./install.sh && eval $cmd && cd .." >> command_list
-    echo "cd ${pcre_ver} && ./configure && make && make install && cd .." >> command_list
-    echo "cd ${zlib_ver} && ./configure && make && make install && cd .." >> command_list
-    for i in ${openssl_packages[@]}; do
-        cmd=$install" "$i
-        echo "$cmd" >> command_list
-    done
-
-    echo "git clone https://github.com/nginx/nginx.git" >> command_list
-    echo "cd nginx" >> command_list
-    echo "git checkout branches/$nginx_branch" >> command_list
-    echo "cd .. && git apply nginx_conf.patch" >> command_list
-    echo "cd nginx" >> command_list
-    cmd="./auto/configure${nginx_paths}${nginx_parametrs}${cc_ld_opt}"
-    echo "$cmd && make && make install" >> command_list
-    if ! [ -d /var/cache/nginx ]
-    then
-        echo "mkdir /var/cache/nginx" >> command_list
-    fi
-
-else
-# ----------------------------------
-    eval "$pkglist | grep -qw gcc"
-    if ! [ "$?" -eq 0 ]
-    then
-        eval "$apt install gcc" || exit 1
-    fi
-    eval "$pkglist | grep \" git \""
-    if ! [ "$?" -eq 0 ]
-    then
-        eval "$apt install git" || exit 1
-    fi
-
-    wget --no-check-certificate -O nginx_conf.patch https://raw.githubusercontent.com/fullincome/scripts/master/nginx-gost/nginx_conf.patch || exit 1
-    wget --no-check-certificate -O ${pcre_ver}.tar.gz ${url}/src/${pcre_ver}.tar.gz && wget --no-check-certificate -O ${zlib_ver}.tar.gz ${url}/src/${zlib_ver}.tar.gz || exit 1
-
-    for i in ${openssl_packages[@]}; do wget --no-check-certificate -O $i ${url}/bin/"${revision_openssl}"/$i || exit 1; done
-    tar -xzvf ${pcre_ver}.tar.gz && tar -xzvf ${zlib_ver}.tar.gz || exit 1
-    if ! [ -d "$csp" ]
-    then
-        if ! [ -d csp ]
-        then
-            mkdir csp
-        fi
-        tar -xzvf $csp -C csp --strip-components 1 || exit 1
-        csp="csp"
-    fi
-
-    cmd=$install" lsb-cprocsp-kc2*"${pkgmsys}
-    cd ${csp} && ./install.sh && eval "$cmd" && cd .. || exit 1
-    cd ${pcre_ver} && ./configure && make && make install && cd .. || exit 1
-    cd ${zlib_ver} && ./configure && make && make install && cd .. || exit 1
-    for i in ${openssl_packages[@]}; do
-        cmd=$install" "$i
-        eval "$cmd" || exit 1
-    done
-
-    # ----------------------------------
-
-    # Установка nginx
-    # ----------------------------------
-
-    git clone https://github.com/nginx/nginx.git
-    cd nginx || exit 1
-    git checkout branches/$nginx_branch || exit 1
-    cd .. && git apply nginx_conf.patch || exit 1
-    cd nginx
-    cmd="./auto/configure${nginx_paths}${nginx_parametrs}${cc_ld_opt}"
-    eval $cmd && make && make install || exit 1
-
-    if ! [ -d /var/cache/nginx ]
-    then
-        mkdir /var/cache/nginx
+        echo "CSP: found"
+    else
+        printf "CSP: not full installed. No argument --csp=[CSP_TGZ/CSP_DIR]\n"
+        exit 0
     fi
 fi
+# ----------------------------------------------
 
+# Проверка GCC
+eval "$pkglist | grep -qw gcc > /dev/null"
+if ! [ "$?" -eq 0 ]
+then
+    gcc_need=true
+else
+    echo "GCC: found"
+fi
+# ----------------------------------------------
+
+# Проверка GIT
+eval "$pkglist | grep \" git \" > /dev/null"
+if ! [ "$?" -eq 0 ]
+then
+    git_need=true
+else
+    echo "GIT: found"
+fi
+# ----------------------------------------------
+
+# Проверка openssl
+if (eval "${pkglist} | grep cpopenssl-110-64 > /dev/null" && \
+    eval "${pkglist} | grep cpopenssl-110-base > /dev/null" && \
+    eval "${pkglist} | grep cpopenssl-110-devel > /dev/null" &&
+    eval "${pkglist} | grep cpopenssl-110-gost-64 > /dev/null")
+then
+    echo "Openssl-1.1.0: found"
+else
+    openssl_need=true
+fi
+# ----------------------------------------------
+
+# Проверка PCRE
+if ! test -e "/usr/local/bin/pcre-config"
+then
+    pcre_need=true
+else
+    echo "PCRE: found"
+fi
+# ----------------------------------------------
+
+# Проверка ZLIB
+if ! test -e "/usr/local/lib/libz.so.1.2.11"
+then
+    zlib_need=true
+else
+    echo "ZLIB: found"
+fi
+
+# Провекра NGINX
+if ! test -e "/usr/sbin/nginx"
+then
+    nginx_need=true
+fi
+# ----------------------------------------------
+
+# ----------------------------------------------
+# ----------------------------------------------
+# ----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ----------------------------------------------
+# ----------Вывод команд в файл-----------------
+# ----------------------------------------------
+if [ ${command_list} == true ];
+then
+    echo "Create command_list"
+    printf "Command list:\n------------------------------\n\n" > command_list
+
+    printf "wget --no-check-certificate -O nginx_conf.patch https://raw.githubusercontent.com/fullincome/scripts/master/nginx-gost/nginx_conf.patch\n\n" >> command_list
+    printf "wget --no-check-certificate -O ${pcre_ver}.tar.gz ${url}/src/${pcre_ver}.tar.gz &&\n" >> command_list
+    printf "wget --no-check-certificate -O ${zlib_ver}.tar.gz ${url}/src/${zlib_ver}.tar.gz\n\n" >> command_list
+
+    for openssl_pkg in ${openssl_packages[@]}; 
+    do 
+        printf "wget --no-check-certificate -O $openssl_pkg ${url}/bin/${revision_openssl}/$openssl_pkg\n" >> command_list
+    done
+    printf "\ntar -xzvf ${pcre_ver}.tar.gz &&\n" >> command_list
+    printf "tar -xzvf ${zlib_ver}.tar.gz\n\n" >> command_list
+    if ! [ -d "$csp" ]
+    then
+        if ! [ -d csp ]
+        then
+            printf "mkdir csp\n" >> command_list
+        fi
+        printf "tar -xzvf $csp -C csp --strip-components 1\n\n" >> command_list
+        csp="csp"
+    fi
+    printf "git clone https://github.com/nginx/nginx.git\n" >> command_list
+    printf "cd nginx &&\n" >> command_list
+    printf "git checkout branches/$nginx_branch\n\n" >> command_list
+
+    if [ ${gcc_need} == true ];
+    then
+        printf "$apt install gcc\n" >> command_list
+    fi
+
+    if [ ${git_need} == true ];
+    then
+        printf "$apt install git\n\n" >> command_list
+    fi
+
+    if [ ${csp_need} == true ];
+    then
+        cmd=$install" lsb-cprocsp-kc2*"${pkgmsys}
+        printf "cd ${csp} && ./install.sh && $cmd && cd ${WORK_PATH}\n" >> command_list
+    fi
+
+    if [ ${pcre_need} == true ];
+    then
+        printf "cd ${pcre_ver} && ./configure && make && make install && cd cd ${WORK_PATH}\n" >> command_list
+    fi
+
+    if [ ${zlib_need} == true ];
+    then
+        printf "${zlib_ver} && ./configure && make && make install && cd cd ${WORK_PATH}\n\n" >> command_list
+    fi
+
+    if [ ${openssl_need} == true ];
+    then
+        for openssl_pkg in ${openssl_packages[@]}; do
+            cmd=$install" "$openssl_pkg
+            printf "$cmd\n" >> command_list
+        done
+    fi
+
+    cmd="./auto/configure${nginx_paths}${nginx_parametrs}${cc_ld_opt}"
+    printf "\ncd ${WORK_PATH} && git apply nginx_conf.patch\n" >> command_list
+    printf "cd nginx &&\n" >> command_list
+    printf "$cmd &&\n" >> command_list
+    printf "make && make install\n" >> command_list
+    if ! [ -d /var/cache/nginx ]
+    then
+        printf "mkdir /var/cache/nginx" >> command_list
+    fi
+
+    exit 0
+fi
+# ----------------------------------------------
+# ----------------------------------------------
+# ----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+# Загрузка и распаковка пакетов
+# Пакеты загружаются все возможные (не имеет значения нужны они или нет)
+if ! [ -e "nginx_conf.patch" ]
+then
+    eval "wget --no-check-certificate -O nginx_conf.patch https://raw.githubusercontent.com/fullincome/scripts/master/nginx-gost/nginx_conf.patch" || exit 1
+fi
+
+if ! [ -e "${pcre_ver}.tar.gz" ]
+then
+    eval "wget --no-check-certificate -O ${pcre_ver}.tar.gz ${url}/src/${pcre_ver}.tar.gz" || exit 1
+fi
+
+if ! [ -e "${pcre_ver}.tar.gz" ]
+then
+    eval "wget --no-check-certificate -O ${zlib_ver}.tar.gz ${url}/src/${zlib_ver}.tar.gz" || exit 1
+fi
+
+for openssl_pkg in ${openssl_packages[@]}; 
+do 
+    if ! [ -e "$openssl_pkg" ]
+    then
+        eval "wget --no-check-certificate -O $openssl_pkg ${url}/bin/${revision_openssl}/$openssl_pkg" || exit 1; 
+    fi
+done
+
+if ! [ -d "$pcre_ver" ]
+then
+    eval "tar -xzvf ${pcre_ver}.tar.gz" || exit 1
+fi
+
+if ! [ -d "$zlib_ver" ]
+then
+eval "tar -xzvf ${zlib_ver}.tar.gz" || exit 1
+fi
+
+if (! test -d "$csp" && test "$csp_need" == "true")
+then
+    if ! [ -d csp ]
+    then
+        mkdir csp
+    fi
+    tar -xzvf $csp -C csp --strip-components 1 || exit 1
+    csp="csp"
+fi
+
+if ! [ -d "nginx" ]
+then
+    echo "Clone nginx repository"
+    git clone https://github.com/nginx/nginx.git || exit 1
+    cd nginx || exit 1
+    echo "Switch branch"
+    git checkout branches/$nginx_branch || exit 1
+fi
+cd ${WORK_PATH}
+# ----------------------------------
+
+# Инсталяция пакетов
+# Инсталируются только необходимые пакеты, которые не нашлись в системе
+if [ ${gcc_need} == true ];
+then
+    echo "Install gcc"
+    eval "$apt install gcc" || exit 1
+fi
+
+if [ ${git_need} == true ];
+then
+    echo "Install git"
+    eval "$apt install git" || exit 1
+fi
+
+if [ ${csp_need} == true ];
+then
+    echo "Install CSP"
+    cmd=$install" lsb-cprocsp-kc2*"${pkgmsys}
+    cd ${csp} && ./install.sh && eval "$cmd" && cd ${WORK_PATH} || exit 1
+fi
+
+if [ ${pcre_need} == true ];
+then
+    echo "Install PCRE"
+    cd ${pcre_ver} && ./configure && make && make install && cd ${WORK_PATH} || exit 1
+fi
+
+if [ ${zlib_need} == true ];
+then
+    echo "Install ZLIB" 
+    cd ${zlib_ver} && ./configure > zlib.log && make > zlib.log && make install > zlib.log && cd ${WORK_PATH} || exit 1
+    eval "rm zlib.log"
+fi
+
+if [ ${openssl_need} == true ];
+then
+    echo "Install Openssl-1.1.0"
+    for openssl_pkg in ${openssl_packages[@]}; do
+        cmd=$install" "$openssl_pkg
+        eval "$cmd > openssl.log" || exit 1
+    done
+    "rm openssl.log"
+fi
+
+# ----------------------------------
+
+# Установка nginx
+if [ ${nginx_need} == true ];
+then
+    echo "Apply patch"
+    cd ${WORK_PATH} && git apply nginx_conf.patch && cd nginx || exit 1
+    
+    cmd="./auto/configure${nginx_paths}${nginx_parametrs}${cc_ld_opt}"
+    echo "Nginx: configure and install"
+    eval $cmd && make && make install || exit 1
+    echo "NGINX: installed"
+else
+    echo "NGINX: installed"
+fi
+
+if ! [ -d /var/cache/nginx ]
+then
+    mkdir /var/cache/nginx
+fi
+cd ${WORK_PATH}
 # ----------------------------------
