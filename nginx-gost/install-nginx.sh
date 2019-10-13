77e67d2cce564a7c1bd773c84821839f4cefad54
fullincome <efremofstepan@yandex.ru>
2019-10-14 01:54:23 +0300

    update: openssl revision 185515

--- nginx-gost/install-nginx.sh
+++ nginx-gost/install-nginx.sh
@@ -6,8 +6,8 @@ WORK_PATH=$(pwd)
 # Пакеты будут скачены с "$url"
 url="https://update.cryptopro.ru/support/nginx-gost"
 
-revision_openssl="180423"
-release_openssl="5.0.11216-5"
+revision_openssl="185515"
+release_openssl="5.0.11315-5"
 pcre_ver="pcre-8.42"
 zlib_ver="zlib-1.2.11"
 
